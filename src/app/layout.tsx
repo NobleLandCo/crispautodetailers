e923212b84de840fef06c111e2988c8cb616ef3b
Josh McCorkle <info@noblelandcompany.com>
2026-08-28 20:12:02 -0700

    Add Google Ads conversion tag AW-18416138303

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,18 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
         />
+        {/* Google Ads conversion tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18416138303" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18416138303');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
